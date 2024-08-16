53c7b5bee208afcae9a85da88728d7b94107310c
= <=>
2024-08-16 21:19:48 +0530

    initial project setup

---
+++ src/main/java/com/example/hazelcastsample/HazelcastSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hazelcastsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HazelcastSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hazelcastsample/HazelcastSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hazelcastsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelcastSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
