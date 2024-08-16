e60e3af9de0120f5278b038b89a5e2e5e1f125e7
= <=>
2024-08-16 21:28:08 +0530

    Cache Controller Created

---
+++ src/main/java/com/example/hazelcastsample/cache/controllers/CacheController.java
@@ -0,0 +1,8 @@
+package com.example.hazelcastsample.cache.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/cache")
+public class CacheController {
+
+}
